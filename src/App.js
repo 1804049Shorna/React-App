553ed4bb3300ff192210ea35f7f80edd9301a483
Shanjida Nahid Shorna <sanjidanahid53@gmail.com>
2023-12-04 22:24:10 +0600

    tutorial 13 done

--- src/App.js
+++ src/App.js
@@ -1,35 +1,58 @@
 import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import Modul from "./components/modul.js";
+import Modul from "./components/Modul.js";
 import TEXTFROM from "./components/Textform.js";
 import About from "./components/About.js";
+import Alert from "./components/Alert.js";
 
 
 function App() {
  const [mode, setDarkMode]=useState("light")
+ const[alert,setAlert]=useState(null);
+ const showAlert=(message ,type)=>{
+  setAlert({
+    msg: message,
+    type:type
+
+  })
+  setTimeout(()=>{
+    setAlert(null);
+
+  },1000
+
+  )
+
+ }
  const togglemode=()=>{
   if(mode==='light')
   {
     setDarkMode('dark');
     document.body.style.backgroundColor='#245eb2'
+    showAlert("Darkmode Has beem enabled","success");
+
 
 
   }
+ 
   else{
     setDarkMode('light');
     document.body.style.backgroundColor='white'
+    showAlert("Lightmode Has beem enabled","success");
   }
    
  }
+ 
+
   return (
     <>
       <Modul title="TEXTAPP" mode={mode} togglemode={togglemode}/>
-
+    <Alert alert={alert}></Alert>
       <div className="container">
         <TEXTFROM heading="Enter your text here" mode={mode} />
         
         {/* <About /> */}
+     
       </div>
     </>
   );
--- src/components/modul.js
+++ src/components/modul.js
@@ -1,70 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function modul(props) {
-  const handlemode=()=>{
-      props.mode="dark";
-  }
-  return (
-    <nav
-      className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}
-    >
-      <div className="container-fluid">
-        <a className="navbar-brand" href="/">
-          {props.title}
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="/navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/">
-                About
-              </a>
-            </li>
-          </ul>
-          {/* <form className="d-flex" role="search">
-            <input
-              className="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button className="btn btn-outline-success" type="submit">
-              Search
-            </button>
-          </form> */}
-          <div className={`form-check form-switch text-${ props.mode==='light' ? 'dark' : 'light'}`}>
-            <input
-              className="form-check-input"
-              type="checkbox"
-              role="switch"
-              id="flexSwitchCheckDefault"
-              onClick={props.togglemode}
-            />
-            <label className="form-check-label" htmlFor="flexSwitchCheckDefault" >
-             Dark Mode
-            </label>
-          </div>
-        </div>
-      </div>
-    </nav>
-  );
+export default function Modul(props) {
+ 
+  const[color ,setColor]=useState('light');
+
+ const handlemode=()=>{
+     props.mode="dark";
+ }
+ const changecolor1=()=>{
+   if(color==='light')
+   {
+      document.body.style.backgroundColor='#ffc107';
+      setColor ('#ffc107');
+   }
+   else if(color==='#ffc107')
+   {
+     document.body.style.backgroundColor='white';
+     setColor('#ffc107')
+   }
+  
+  
+ }
+ const changecolor2=()=>{
+   document.body.style.backgroundColor='#d63384';
+ }
+ return (
+   <nav
+     className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}
+   >
+     <div className="container-fluid">
+       <a className="navbar-brand" href="/">
+         {props.title}
+       </a>
+       <button
+         className="navbar-toggler"
+         type="button"
+         data-bs-toggle="collapse"
+         data-bs-target="/navbarSupportedContent"
+         aria-controls="navbarSupportedContent"
+         aria-expanded="false"
+         aria-label="Toggle navigation"
+       >
+         <span className="navbar-toggler-icon"></span>
+       </button>
+       <div className="collapse navbar-collapse" id="navbarSupportedContent">
+         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+           <li className="nav-item">
+             <a className="nav-link active" aria-current="page" href="/">
+               Home
+             </a>
+           </li>
+           <li className="nav-item">
+             <a className="nav-link" href="/">
+               About
+             </a>
+           </li>
+         </ul>
+         <button className="Newbtn1" onClick={changecolor1}>Green</button>
+         <button className="Newbtn2" onClick={changecolor2}>Yellow</button>
+         <button className="Newbtn3"></button>
+
+         <div className={`form-check form-switch text-${ props.mode==='light' ? 'dark' : 'light'}`}>
+           <input
+             className="form-check-input"
+             type="checkbox"
+             role="switch"
+             id="flexSwitchCheckDefault"
+             onClick={props.togglemode}
+           />
+           <label className="form-check-label" htmlFor="flexSwitchCheckDefault" >
+            Dark Mode
+           </label>
+         </div>
+       </div>
+     </div>
+   </nav>
+ );
 }
 
-modul.defaultProps = {
-  title: "Something Else",
+Modul.defaultProps = {
+ title: "Something Else",
 };
