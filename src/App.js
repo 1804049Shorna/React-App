1ed35d4e695ce20bcc5d9730e91f61188e98f60b
Shanjida Nahid Shorna <sanjidanahid53@gmail.com>
2023-12-05 20:17:48 +0600

    Tutorial !7 done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+/**import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Modul from "./components/Modul.js";
@@ -6,56 +6,109 @@ import TEXTFROM from "./components/Textform.js";
 import About from "./components/About.js";
 import Alert from "./components/Alert.js";
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+  Router,
+  Routes, // Import Routes
+} from "react-router-dom";
 
 function App() {
- const [mode, setDarkMode]=useState("light")
- const[alert,setAlert]=useState(null);
- const showAlert=(message ,type)=>{
-  setAlert({
-    msg: message,
-    type:type
-
-  })
-  setTimeout(()=>{
-    setAlert(null);
-
-  },1000
-
-  )
-
- }
- const togglemode=()=>{
-  if(mode==='light')
-  {
-    setDarkMode('dark');
-    document.body.style.backgroundColor='#245eb2'
-    showAlert("Darkmode Has beem enabled","success");
-
-
-
-  }
- 
-  else{
-    setDarkMode('light');
-    document.body.style.backgroundColor='white'
-    showAlert("Lightmode Has beem enabled","success");
-  }
-   
- }
- 
+  const [mode, setDarkMode] = useState("light");
+  const [alert, setAlert] = useState(null);
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1000);
+  };
+  const togglemode = () => {
+    if (mode === "light") {
+      setDarkMode("dark");
+      document.body.style.backgroundColor = "#245eb2";
+      showAlert("Darkmode Has beem enabled", "success");
+    } else {
+      setDarkMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Lightmode Has beem enabled", "success");
+    }
+  };
 
   return (
     <>
-      <Modul title="TEXTAPP" mode={mode} togglemode={togglemode}/>
-    <Alert alert={alert}></Alert>
-      <div className="container">
-        <TEXTFROM heading="Enter your text here" mode={mode} />
-        
-        {/* <About /> */}
-     
-      </div>
+      <Router>
+        <Modul title="TEXTAPP" mode={mode} togglemode={togglemode} />
+        <Alert alert={alert}></Alert>
+        <div className="container">
+        <Routes>
+           <Route path="/about" element={<About />} />
+           <Route path="/" element={<TEXTFROM heading="Enter your text here" mode={mode} />} />
+         </Routes>
+        </div>
+      </Router>
     </>
   );
 }
 
+export default App; /** */
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Modul from "./components/Modul.js";
+import TEXTFROM from "./components/Textform.js";
+import About from "./components/About.js";
+import Alert from "./components/Alert.js";
+import {
+ BrowserRouter as Router, // Import BrowserRouter
+ Route,
+ Link,
+ Routes, // Import Routes
+} from "react-router-dom";
+
+function App() {
+ const [mode, setDarkMode] = useState("light");
+ const [alert, setAlert] = useState(null);
+ const showAlert = (message, type) => {
+  setAlert({
+    msg: message,
+    type: type,
+  });
+  setTimeout(() => {
+    setAlert(null);
+  }, 1000);
+ };
+ const togglemode = () => {
+  if (mode === "light") {
+    setDarkMode("dark");
+    document.body.style.backgroundColor = "#245eb2";
+    showAlert("Darkmode Has beem enabled", "success");
+  } else {
+    setDarkMode("light");
+    document.body.style.backgroundColor = "white";
+    showAlert("Lightmode Has beem enabled", "success");
+  }
+ };
+
+ return (
+  <>
+    <Router> {/* Use BrowserRouter */}
+      <Modul title="TEXTAPP" mode={mode} togglemode={togglemode} />
+      <Alert alert={alert}></Alert>
+      <div className="container">
+        <Routes>
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/" element={<TEXTFROM heading="Enter your text here" mode={mode} />} />
+        </Routes>
+      </div>
+    </Router>
+  </>
+ );
+}
+
 export default App;
+
--- src/components/modul.js
+++ src/components/modul.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import './Modul.css';
+import { Link } from "react-router-dom";
 
 export default function Modul(props) {
  
@@ -48,9 +49,9 @@ export default function Modul(props) {
      className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}
    >
      <div className="container-fluid">
-       <a className="navbar-brand" href="/">
+       <Link className="navbar-brand" to="/">
          {props.title}
-       </a>
+       </Link>
        <button
          className="navbar-toggler"
          type="button"
@@ -65,14 +66,14 @@ export default function Modul(props) {
        <div className="collapse navbar-collapse" id="navbarSupportedContent">
          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
            <li className="nav-item">
-             <a className="nav-link active" aria-current="page" href="/">
+             <Link className="nav-link active" aria-current="page" to="/">
                Home
-             </a>
+             </Link>
            </li>
            <li className="nav-item">
-             <a className="nav-link" href="/">
+             <Link className="nav-link" to="/about">
                About
-             </a>
+             </Link>
            </li>
          </ul>
          <button className="Newbtn1" onClick={changecolor1}></button>
